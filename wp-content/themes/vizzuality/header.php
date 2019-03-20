4ca5f8977ebbd80f31dbdce9f5f4f736c1a4d05f
Tiago Garcia <tiagojsag@gmail.com>
2019-03-20 13:38:57 +0100

    Add google analytics tracking

--- wp-content/themes/vizzuality/header.php
+++ wp-content/themes/vizzuality/header.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
 <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6363508-12"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-6363508-12');
+    </script>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head() ?>
